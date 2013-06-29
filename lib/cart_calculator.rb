0a709fc2cb29eb3a96e739dc7c89b86cd2a918a6
Uroš Jurglič <jurglic@antiveb.com>
2013-06-29 16:47:41 +0200

    Refactor promotions merging logic and extract it into private method.

--- lib/cart_calculator.rb
+++ lib/cart_calculator.rb
@@ -20,9 +20,7 @@ class CartCalculator
     # here we merge multiple promotions into aggregate discount
     # this could be extracted into service object in the future to provide
     # more detailed control over merging of overlapping promotions
-    total_discount = @cart_total_promotions.map(&:discount).reduce(:+) || 0
-    # prevent total discount being larger than 100%
-    total_discount = [total_discount, 1].min
+    total_discount = merge_multiple_promotion_discounts(@cart_total_promotions)
     # round according to spec to 2 decimal places
     (total_amount_wo_promotions * (1 - total_discount)).round(2)
   end
@@ -33,13 +31,10 @@ class CartCalculator
       # this could be extracted into service object in the future to provide
       # more detailed control over merging of overlapping promotions
       promotions = @cart_item_promotions[item.product] || []
-      item_discount = promotions.map(&:discount).reject(&:nil?).reduce(:+) || 0
-      # prevent discount being larger than 100%
-      item_discount = [item_discount, 1].min
+      item_discount = merge_multiple_promotion_discounts(promotions)     
 
       # if promotion defines new price instead of a discount, best price is chosen
-      item_prices = promotions.map(&:discounted_price).reject(&:nil?) || []
-      best_price = item_prices.push(item.product.price).min
+      best_price = merge_multiple_promotion_prices(promotions, item.product.price)
 
       # final item price
       best_price * (1 - item_discount) * item.quantity
@@ -71,6 +66,19 @@ class CartCalculator
         @cart_total_promotions << promotion
       end
     end
+  end
+
+  def merge_multiple_promotion_discounts(promotions)
+    discounts = promotions.map(&:discount).reject(&:nil?)
+    merged_discount = discounts.reduce(:+) || 0    
+    # prevent discount being larger than 100%
+    [merged_discount, 1].min
+  end
+
+  def merge_multiple_promotion_prices(promotions, base_price)
+    item_prices = promotions.map(&:discounted_price).reject(&:nil?)
+    item_prices ||= []
+    item_prices.push(base_price).min
   end
 
 end
